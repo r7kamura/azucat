9684386ea8f04e936d1a435cb020a76881864420
Ryo NAKAMURA <r7kamura@gmail.com>
2012-01-31 13:57:33 +0900

    azucat.rbはrequire担当

--- lib/azucat.rb
+++ lib/azucat.rb
@@ -12,40 +12,9 @@ rescue Bundler::GemNotFound => e
 end
 
 require "azucat/ext"
+require "azucat/core"
 require "azucat/http_server"
 require "azucat/http_app"
 require "azucat/websocket_server"
 require "azucat/output"
 require "azucat/browser"
-
-module Azucat
-  extend self
-
-  def start(opts = {})
-    opts = {
-      :log_size     => 100,
-      :ws_port      => unused_port,
-      :ws_host      => "localhost",
-      :http_port    => unused_port,
-      :http_host    => "localhost",
-      :open_browser => true,
-      :channel      => EM::Channel.new
-    }.merge(opts)
-
-    Thread.abort_on_exception = true
-    EM.run do
-      EM.defer { HTTPServer.run(opts)      }
-      EM.defer { WebSocketServer.run(opts) }
-      EM.defer { Output.run(opts)          }
-      EM.defer { Browser.open(opts)        }
-    end
-  end
-
-  private
-  def unused_port
-    s = ::TCPServer.open(0)
-    port = s.addr[1]
-    s.close
-    port
-  end
-end
---
+++ lib/azucat/core.rb
@@ -0,0 +1,33 @@
+module Azucat
+  module Core
+    def start(opts = {})
+      opts = {
+        :log_size     => 100,
+        :ws_port      => unused_port,
+        :ws_host      => "localhost",
+        :http_port    => unused_port,
+        :http_host    => "localhost",
+        :open_browser => true,
+        :channel      => EM::Channel.new
+      }.merge(opts)
+
+      Thread.abort_on_exception = true
+      EM.run do
+        EM.defer { HTTPServer.run(opts)      }
+        EM.defer { WebSocketServer.run(opts) }
+        EM.defer { Output.run(opts)          }
+        EM.defer { Browser.open(opts)        }
+      end
+    end
+
+    private
+    def unused_port
+      s = ::TCPServer.open(0)
+      port = s.addr[1]
+      s.close
+      port
+    end
+  end
+
+  extend Core
+end
