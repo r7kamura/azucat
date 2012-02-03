ee6bd19054320238693b3d873200dfa56053444f
Ryo NAKAMURA <r7kamura@gmail.com>
2012-02-04 03:17:06 +0900

    URLにリンク付け

--- public/js/azucat.js
+++ public/js/azucat.js
@@ -1,25 +1,33 @@
 /*
-* Example:
+* example:
 *   <script type="text/javascript" src="/js/jquery.min.js"></script>
 *   <script type="text/javascript" src="/js/azucat.js"></script>
-*   <script type="text/javascript"> Azucat.start({port: 3000}); </script>
+*   <script type="text/javascript"> Azucat.start(3000); </script>
 * */
 var Azucat = {
   start: function(port) {
     var self = this;
     $(function() {
-      var ws   = new WebSocket('ws://localhost:' + port);
+      var ws = new WebSocket('ws://localhost:' + port);
       ws.onmessage = function(e) {
         var isScrolledToBottom = self._isScrolledToBottom();
-        self._appendToBody(e.data);
+
         self._updateTitle(e.data);
+        self._appendToBody(e.data);
+
         if (isScrolledToBottom) { self._scrollToBottom() }
       };
     });
   },
 
+  _addLink: function(str) {
+    return str.replace(/(http:\/\/[\x21-\x7e]+)/gi, '<a href="$1">$1</a>');
+  },
+
   _appendToBody: function(str) {
-    $(document.body).append('<pre>' + str + '</pre>');
+    str = this._addLink(str)
+    str = '<pre>' + str + '</pre>';
+    $(document.body).append(str)
   },
 
   _updateTitle: function(str) {
