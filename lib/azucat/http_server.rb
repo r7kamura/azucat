0b5df8b41b1017ed733c2fcc413fa2ce9768d653
Ryo NAKAMURA <r7kamura@gmail.com>
2012-02-10 00:11:05 +0900

    リファクタリング

--- lib/azucat/http_server.rb
+++ lib/azucat/http_server.rb
@@ -1,22 +1,31 @@
-class Azucat::HTTPServer
-  def self.run
-    ::Rack::Handler::WEBrick.run(
-      Class.new(Sinatra::Base) {
-        set :root, File.expand_path("../../../", __FILE__)
-        set :ws_port, Azucat.config.ws_port
+module Azucat
+  module HTTPServer
+    extend self
 
-        get "/" do
-          @ws_port = settings.ws_port
-          erb :index
-        end
+    def run
+      ::Rack::Handler::WEBrick.run(
+        Class.new(Sinatra::Base) {
+          set :root, File.expand_path("../../../", __FILE__)
+          set :ws_port, Azucat.config.ws_port
 
-        post "/" do
-          ap params
-        end
-      },
-      :Port      => Azucat.config.http_port,
-      :Logger    => ::WEBrick::Log.new("/dev/null"),
-      :AccessLog => [nil, nil]
-    )
+          get "/" do
+            @ws_port = settings.ws_port
+            erb :index
+          end
+
+          post "/" do
+            tweet(params[:command])
+          end
+        },
+        :Port      => Azucat.config.http_port,
+        :Logger    => ::WEBrick::Log.new("/dev/null"),
+        :AccessLog => [nil, nil]
+      )
+    end
+
+    private
+    def tweet(str)
+      Twitter.tweet(params[:command])
+    end
   end
 end
--- lib/azucat/twitter.rb
+++ lib/azucat/twitter.rb
@@ -1,12 +1,28 @@
 module Azucat
-  class Twitter
+  module Twitter
+    extend self
 
     Azucat.init do
       next unless Azucat.config.twitter
+      setup_config
+      setup_client
+      Azucat::Output.notify { |str| str.match("@" + @info["screen_name"]) }
+    end
 
-      if Azucat.config.access_key.blank? || Azucat.config.access_secret.blank?
-        get_access_token
-      end
+    def run
+      start_stream if Azucat.config.twitter
+    end
+
+    def tweet(str)
+      @client.update(str)
+    end
+
+    private
+
+    def setup_config
+      config = Azucat.config
+      get_access_token unless config.access_key and config.access_secret
+
       @config = {
         :host  => 'userstream.twitter.com',
         :path  => '/2/user.json',
@@ -15,30 +31,27 @@ module Azucat
           :access_key      => nil,
           :access_secret   => nil,
           :proxy           => ENV["http_proxy"],
-          :consumer_key    => Azucat.config.consumer_key,
-          :consumer_secret => Azucat.config.consumer_secret,
-          :access_key      => Azucat.config.access_key,
-          :access_secret   => Azucat.config.access_secret
+          :consumer_key    => config.consumer_key,
+          :consumer_secret => config.consumer_secret,
+          :access_key      => config.access_key,
+          :access_secret   => config.access_secret
         }
       }
+    end
 
-      @screen_name = ::TwitterOAuth::Client.new(
+    def setup_client
+      @client = ::TwitterOAuth::Client.new(
         :consumer_key    => @config[:oauth][:consumer_key],
         :consumer_secret => @config[:oauth][:consumer_secret],
         :token           => @config[:oauth][:access_key],
         :secret          => @config[:oauth][:access_secret]
-      ).info["screen_name"]
-
-      Azucat::Output.notify do |filtered|
-        filtered.match("@" + @screen_name)
-      end
+      )
+      @info = @client.info
     end
 
-    def self.run
-      return unless Azucat.config.twitter
-
+    def start_stream
       stream = ::Twitter::JSONStream.connect(@config)
-      stream.each_item         { |item| Output.puts convert_to_str(item) }
+      stream.each_item         { |item| Output.puts parse_tweet(item)    }
       stream.on_error          { |msg|  Output.puts "Error: #{msg}"      }
       stream.on_reconnect      {        Output.puts "Reconnect"          }
       stream.on_max_reconnects {        Output.puts "Failed"             }
@@ -46,43 +59,58 @@ module Azucat
       Output.puts("[ERROR] (#{self}) #{e.message}")
     end
 
-    private
-    def self.get_access_token
-      request_token = OAuth::Consumer.new(
+    def get_access_token
+      please_open_authorize_url
+      please_enter_the_pin
+      get_and_save_access_token
+    end
+
+    def please_open_authorize_url(msg)
+      puts "1) Open: #{request_token.authorize_url}"
+      ::Launchy.open(request_token.authorize_url) rescue nil
+    end
+
+    def please_enter_the_pin
+      print "2) Enter the PIN > "
+      @pin = STDIN.gets.strip
+    end
+
+    def get_and_save_access_token
+      Azucat.config[:access_key]    = access_token.token
+      Azucat.config[:access_secret] = access_token.secret
+      write_config
+    end
+
+    def request_token
+      @request_token ||= OAuth::Consumer.new(
         Azucat.config[:consumer_key],
         Azucat.config[:consumer_secret],
         :site  => "https://api.twitter.com",
         :proxy => ENV["http_proxy"],
         :ssl   => true
       ).get_request_token
-
-      puts "1) open: #{request_token.authorize_url}"
-      ::Launchy.open(request_token.authorize_url) rescue nil
-
-      puts "2) Enter the PIN"
-      pin = STDIN.gets.strip
-
-      access_token = request_token.get_access_token(:oauth_verifier => pin)
-      config = Azucat.config
-      config[:access_key]    = access_token.token
-      config[:access_secret] = access_token.secret
-      puts %{Saving 'token' and 'secret' to '#{config[:file]}'}
-      File.open(Azucat.config[:file], "a") do |f|
-        f.puts %{Azucat.config[:access_key]    = '#{config[:access_key]}'}
-        f.puts %{Azucat.config[:access_secret] = '#{config[:access_secret]}'}
-      end
-      access_token
     end
 
-    def self.convert_to_str(tweet_json)
-      hash = JSON.parse(tweet_json)
-      return unless hash["user"]
+    def access_token
+      @access_token ||= request_token.get_access_token(:oauth_verifier => @pin)
+    end
 
-      Azucat::Output.stringify(
-        :name => hash["user"]["screen_name"],
-        :tag  => "TWIT",
-        :text => hash["text"]
-      )
+    def write_config
+      puts "Saving token and secret to #{Azucat.config[:file]}"
+      File.open(Azucat.config[:file], "a") do |f|
+        f.puts %{Azucat.config[:access_key]    = '#{Azucat.config[:access_key]}'}
+        f.puts %{Azucat.config[:access_secret] = '#{Azucat.config[:access_secret]}'}
+      end
+    end
+
+    def parse_tweet(tweet_json)
+      tweet = JSON.parse(tweet_json)
+      return unless tweet["user"]
+      {
+        :name => tweet["user"]["screen_name"],
+        :text => tweet["text"],
+        :tag  => "TWIT"
+      }
     end
   end
 end
